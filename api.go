f6e9993f06494c4811df39542ad7fc9e0791e4b2
Zhaoqi Tong <zhaoqi.tong@outlook.com>
2023-12-26 13:11:37 +0800

    refactor: remove `atomic` for default eventbus

--- api.go
+++ api.go
@@ -2,23 +2,18 @@ package eventbus
 
 import (
 	"context"
-	"sync/atomic"
 )
 
-var defaultEventBus atomic.Pointer[EventBus]
-
-func init() {
-	defaultEventBus.Store(New(NewInMemoryHandler()))
-}
+var defaultEventBus = New(NewInMemoryHandler())
 
 func SetDefault(eventbus *EventBus) {
-	defaultEventBus.Store(eventbus)
+	defaultEventBus = eventbus
 }
 
 func Subscribe(ctx context.Context, source Source, callback Callback) error {
-	return defaultEventBus.Load().Subscribe(ctx, source, callback)
+	return defaultEventBus.Subscribe(ctx, source, callback)
 }
 
 func Publish(ctx context.Context, source Source, payload Payload) error {
-	return defaultEventBus.Load().Publish(ctx, source, payload)
+	return defaultEventBus.Publish(ctx, source, payload)
 }
